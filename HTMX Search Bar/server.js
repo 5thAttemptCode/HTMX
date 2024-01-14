a89cbc7432d6e780280c1b34a165f53d5c06adb3
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-14 23:15:22 +0100

    Add files via upload

--- HTMX Search Bar/server.js
+++ HTMX Search Bar/server.js
@@ -1,68 +1,9 @@
 import express from "express"
-
 const app = express()
 
 app.use(express.static("public"))
-
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.json())
-
-//Handle GET
-app.get("/users", async (req, res) =>  {
-    // const users = [
-    //     {id: 1, name: "Henry Wilkens"},
-    //     {id: 2, name: "Dory Smith"},
-    //     {id: 3, name: "Eva Mendoza"},
-    // ]
-
-    setTimeout(async () => {
-        const limit = +req.query.limit || 10
-
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`)
-        const users = await response.json()
-    
-        res.send(`
-            <h1 class="text-2xl font-bold my-4">Users</h1>
-            <ul>
-                ${users.map((user) => `<li>${user.name}</li>`).join("")}
-            </ul>
-        `)
-    }, 2000)
-
-})
-
-//Handle POST
-app.post("/convert", (req, res) => {
-    setTimeout(() => {
-        const fahrenheit = parseFloat(req.body.fahrenheit)
-        const celcius = (fahrenheit - 32) * (5 / 9)
-
-        res.send(`
-            <p>
-                ${fahrenheit} degrees Fahrenheit is equal to ${celcius.toFixed(1)} degrees Celcius
-            </p>
-        `)
-    }, 2000)
-})
-
-
-let counter = 0
-//Handle GET for polling
-app.get("/poll" , (req, res) => {
-    counter++
-    const data = {value: counter}
-
-    res.json(data)
-})
-
-
-let currentTemperature = 20
-
-app.get("/get-temperature", (req, res) => {
-    currentTemperature += Math.random() * 2 - 1
-    res.send(currentTemperature.toFixed(1) + "°C")
-})
 
 
 const contacts = [
@@ -71,22 +12,24 @@ const contacts = [
     { name: 'Alice Smith', email: 'alice@example.com' },
     { name: 'Bob Williams', email: 'bob@example.com' },
     { name: 'Mary Harris', email: 'mary@example.com' },
-    { name: 'David Mitchell', email: 'david@example.com' },
+    { name: 'David Mitchell', email: 'david@example.com' }
 ]
 
 //Handle POST for user search
-app.post("/search", (req, res) => {
-    const searchTerm = req-body.search.toLowerCase()
+app.post('/search', (req, res) => {
+    const searchTerm = req.body.search.toLowerCase()
 
     if(!searchTerm){
-        return res.send("<tr></tr>")
+        return res.send('<tr></tr>')
     }
 
     const searchResults = contacts.filter((contact) => {
         const name = contact.name.toLowerCase()
         const email = contact.email.toLowerCase()
 
-        return name.includes(searchTerm) || email.includes(searchTerm)
+        return (
+            name.includes(searchTerm) || email.includes(searchTerm)
+        )
     })
 
     setTimeout(() => {
@@ -95,11 +38,12 @@ app.post("/search", (req, res) => {
             <td><div class="my-4 p-2">${contact.name}</div></td>
             <td><div class="my-4 p-2">${contact.email}</div></td>
         </tr>
-        `).join("")
+        `).join('')
 
         res.send(searchResultHtml)
     }, 1000)
 })
+
 
 app.listen(3000, () => {
     console.log(`Server is running on port:3000`)
