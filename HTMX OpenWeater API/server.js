2fc3e5bee08d79d7d41429ace5498ebe47fcc319
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-01 09:24:53 +0100

    Add files via upload

--- HTMX OpenWeater API/server.js
+++ HTMX OpenWeater API/server.js
@@ -1,31 +1,30 @@
-import express from "express";
-import axios from 'axios';
-import dotenv from 'dotenv';
+import express from "express"
+import axios from 'axios'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(express.static("public"));
+const app = express()
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.send('')
-});
+})
   
 app.get("/weather", async (req, res) => {
     const city = req.query.city
     const apiKey = process.env.API_KEY
-    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apiKey}`
+    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     try {
         const response = await axios.get(APIUrl)
-        res.setHeader('Content-Type', 'text/html')
-        res.send(`<div>Weather: ${response.data.weather[0].main}</div>`)
+        const temperature = response.data.main.temp
+        res.send(`The temperature in ${city} is ${temperature}°C`)
     } catch (error) {
-        res.setHeader('Content-Type', 'text/html')
-        res.send(`<div>${city}</div>`)
         console.log(error)
+        res.send(`Could not find temperature for ${city}`)
     }
-});
+})
 
 app.listen(3000, () => {
     console.log(`Server is running on port:3000`)
-});
+})
